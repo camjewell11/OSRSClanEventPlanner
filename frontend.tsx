93946e79734ab731459ff08b2b7893a6142f9f95
clashoftornados <clashoftornados@hotmail.com>
2025-06-16 21:18:39 -0500

    Move to using react for using chakra ui

--- frontend.tsx
+++ frontend.tsx
@@ -1,9 +1,10 @@
 import 'bootstrap';
-import {h, render} from 'preact';
 import PlayerCard from './player_card';
+import * as ReactDOM from 'react-dom/client';
 
 const app = document.getElementById("app");
 if (app != null)
 {
-    render(<PlayerCard level={32} account_type={"MAIN"} username={"test"} ca_score={0} hours={"40 - 50"} timezone ={"EST"}></PlayerCard>, app);
+    const root = ReactDOM.createRoot(app);
+    root.render(<PlayerCard level={2277} account_type={"MAIN"} username={"test"} ca_score={2525} hours={"40 - 50"} timezone ={"EST"} has_shadow={true} notes="test"></PlayerCard>, app);
 }
